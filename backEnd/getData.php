b691b126f6aa69105f4a5455a4b8a96c08886b1b
twinkle8s <twinklecindy8s@gmail.com>
2016-04-30 15:51:17 +0800

    get data by month finish

--- backEnd/getData.php
+++ backEnd/getData.php
@@ -1,9 +1,42 @@
 <?php
-function getDataByMonth($item)
-$jsondata = file_get_contents("http://m.coa.gov.tw/OpenData/FarmTransData.aspx?Crop=");
-$json = json_decode($jsondata, true);
-foreach ($json as $time) 
-{
-	echo $time['交易日期'];
+if (isset($_POST["crop"]) && isset($_POST["year"])) {
+	getDataByMonth($_POST["crop"]);
+}
+else {
+	echo "error";
+}
+
+function getDataByMonth($crop) {
+	switch ($crop) {
+		case '甘藍':
+			$filename = 'CabbageByMonth.json';
+			break;
+
+		case '胡瓜':
+			$filename = 'CucumberByMonth.json';
+			break;
+		
+		case '菜豆':
+			$filename = 'BeanByMonth';
+			break;
+
+		default:
+			$filename = 'error';
+			break;
+	}
+
+	$jsondata = file_get_contents($filename);
+	$json = json_decode($jsondata, true);
+	for($i=0; $i<12; $i++) {
+		echo $json['month'][$i]." ";
+	}
+	echo "<br>";
+	for($i=0; $i<12; $i++) {
+		echo $json['avg_price'][$i]." ";
+	}
+	echo "<br>";
+	for($i=0; $i<12; $i++) {
+		echo $json['avg_trade'][$i]." ";
+	}
 }
 ?>
