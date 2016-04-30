b9b057312b646b3ac524177fd23fa37994bf31b3
twinkle8s <twinklecindy8s@gmail.com>
2016-05-01 01:23:54 +0800

    draw finish

--- frontEnd/graph.js
+++ frontEnd/graph.js
@@ -7,13 +7,22 @@ $(function() {
 
             url: "../backEnd/getData.php",
             type: "POST",
-            data: {veg: $("#vegetable").val(),
-                range: $("#range").val()},
+            data: {veg1: $("#vegetable1").val(),
+                veg2: $("#vegetable2").val(),
+                range: $('input[name=range]:checked').val(),
+                month: $('select[name=month]').val()
+            },
             dataType: "json",
 
             success: function(msg) {
                 console.log(msg);
-                draw(msg.month);
+                if ($('input[name=range]:checked').val() == "year") {
+                    draw("canvas", msg);
+                }
+                else {
+                    draw("canvas", msg.byMonth);
+                    draw("canvas2", msg);
+                }
             },
 
             error: function(msg) {
@@ -23,84 +32,107 @@ $(function() {
         });
     });
 
-    function draw(month) {
-    var barChartData = {
-        labels: month,
-        datasets: [{
-        type: 'bar',
-              label: "Visitor",
-                data: [100, 185, 590, 621, 250, 400, 95],
-                fill: false,
-                backgroundColor: '#71B37C',
-                borderColor: '#71B37C',
-                hoverBackgroundColor: '#71B37C',
-                hoverBorderColor: '#71B37C',
-                yAxisID: 'y-axis-1'
-        }, {
-            type:'line',
-                label: "Sales",
-                data: [51, 65, 40, 49, 60, 37, 40],
-                fill: false,
-                tension:0,
-                borderColor: '#EC932F',
-                backgroundColor: '#EC932F',
-                pointBorderColor: '#EC932F',
-                pointBackgroundColor: '#EC932F',
-                pointHoverBackgroundColor: '#EC932F',
-                pointHoverBorderColor: '#EC932F',
-                yAxisID: 'y-axis-2'
-        }]
-    };
-
-    var ctx = document.getElementById("canvas").getContext("2d");
-    window.myBar = new Chart(ctx, {
-        type: 'bar',
-        data: barChartData,
-        options: {
-            responsive: true,
-            tooltips: {
-                mode: 'label'
-            },
-            elements: {
-                line: {
-                    fill: false
-                }
-            },
-            scales: {
-                xAxes: [{
-                    display: true,
-                    gridLines: {
-                        display: false
-                    },
-                    labels: {
-                        show: true,
-                    }
-                }],
-                yAxes: [{
-                    type: "linear",
-                    display: true,
-                    position: "left",
-                    id: "y-axis-1",
-                    gridLines:{
-                        display: false
-                    },
-                    labels: {
-                        show:true,
-                    }
+    function draw(place, alldata) {
+        var barChartData = {
+            labels: alldata.x_axis,
+            datasets: [{
+                type: 'bar',
+                  label: "胡瓜平均量",
+                    data: alldata.cucumber_trade,
+                    fill: false,
+                    backgroundColor: '#71B37C',
+                    borderColor: '#71B37C',
+                    hoverBackgroundColor: '#71B37C',
+                    hoverBorderColor: '#71B37C',
+                    yAxisID: 'y-axis-1'
                 }, {
-                    type: "linear",
-                    display: true,
-                    position: "right",
-                    id: "y-axis-2",
-                    gridLines:{
-                        display: false
-                    },
-                    labels: {
-                        show:true,
+                type: 'bar',
+                  label: "菜豆平均量",
+                    data: alldata.bean_trade,
+                    fill: false,
+                    backgroundColor: '#71B37C',
+                    borderColor: '#71B37C',
+                    hoverBackgroundColor: '#71B37C',
+                    hoverBorderColor: '#71B37C',
+                    yAxisID: 'y-axis-1'
+                }, {
+                type:'line',
+                    label: "胡瓜平均價",
+                    data: alldata.cucumber_price,
+                    fill: false,
+                    tension:0,
+                    borderColor: '#EC932F',
+                    backgroundColor: '#EC932F',
+                    pointBorderColor: '#EC932F',
+                    pointBackgroundColor: '#EC932F',
+                    pointHoverBackgroundColor: '#EC932F',
+                    pointHoverBorderColor: '#EC932F',
+                    yAxisID: 'y-axis-2'
+                }, {
+                type:'line',
+                    label: "菜豆平均價",
+                    data: alldata.bean_price,
+                    fill: false,
+                    tension:0,
+                    borderColor: '#EC932F',
+                    backgroundColor: '#EC932F',
+                    pointBorderColor: '#EC932F',
+                    pointBackgroundColor: '#EC932F',
+                    pointHoverBackgroundColor: '#EC932F',
+                    pointHoverBorderColor: '#EC932F',
+                    yAxisID: 'y-axis-2'
+            }]
+        };
+
+        var ctx = document.getElementById(place).getContext("2d");
+        window.myBar = new Chart(ctx, {
+            type: 'bar',
+            data: barChartData,
+            options: {
+                responsive: true,
+                tooltips: {
+                    mode: 'label'
+                },
+                elements: {
+                    line: {
+                        fill: false
                     }
-                }]
+                },
+                scales: {
+                    xAxes: [{
+                        display: true,
+                        gridLines: {
+                            display: false
+                        },
+                        labels: {
+                            show: true,
+                        }
+                    }],
+                    yAxes: [{
+                        type: "linear",
+                        display: true,
+                        position: "left",
+                        id: "y-axis-1",
+                        gridLines:{
+                            display: false
+                        },
+                        labels: {
+                            show:true,
+                        }
+                    }, {
+                        type: "linear",
+                        display: true,
+                        position: "right",
+                        id: "y-axis-2",
+                        gridLines:{
+                            display: false
+                        },
+                        labels: {
+                            show:true,
+                        }
+                    }]
+                }
             }
-        }
-    });
-}
+        });
+    }
 });
