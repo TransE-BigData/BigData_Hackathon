80bd02dd8afb9d239d7702a2892cca741b523cdd
HanVincent <b84414@gmail.com>
2016-04-30 15:32:31 +0800

    Form submit content

--- frontEnd/page.js
+++ frontEnd/page.js
@@ -17,35 +17,22 @@ $(document).ready(function() {
             monthExists = true;
             yearExists = false;
 
-            var content = "<span id='month'>請選擇月份：<select>";
-            for (var i = 1; i <= 12; i++)
-                content += "<option value='" + i + "'>" + i + "月</option>";
-            content += "</select></span>";
+            showSelect("月", "month", 1, 12);
 
         } else if (selectedRange === "year" && !yearExists && monthExists) { //if user choose year
             $("#month").remove();
             yearExists = true;
             monthExists = false;
 
-            var content = "<span id='year'>請選擇年份：<select>";
-            for (var i = 2011; i <= 2016; i++)
-                content += "<option value='" + i + "'>" + i + "年</option>";
-            content += "</select></span>";
-
-            isMonthOrYear = 0;
+            showSelect("年", "year", 2011, 2016);
         }
-
-        $("#monthOrYear").append(content);
     });
-
-
-
 
 
 });
 
 function showSelect(range, engRange, start, end) {
-    var content = "<span id='" + engRange + "'>請選擇" + range + "份：<select>";
+    var content = "<span id='" + engRange + "'>請選擇" + range + "份：<select name=" + engRange + ">";
     for (var i = start; i <= end; i++)
         content += "<option value='" + i + "'>" + i + range + "</option>";
     content += "</select></span>";
