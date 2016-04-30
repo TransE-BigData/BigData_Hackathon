2ffb41085ab1c11bade7a2f0176038d862e9886e
HanVincent <b84414@gmail.com>
2016-04-30 15:22:57 +0800

    A little adjustment

--- frontEnd/page.js
+++ frontEnd/page.js
@@ -2,20 +2,10 @@ $(document).ready(function() {
     var monthExists = $('input[name="range"]:checked', '#myForm').val() === 'month' ? true : false;
     var yearExists = $('input[name="range"]:checked', '#myForm').val() === 'year' ? true : false;
 
-    if (monthExists) {
-        var content = "<span id='month'>請選擇月份：<select>";
-        for (var i = 1; i <= 12; i++)
-            content += "<option value='" + i + "'>" + i + "月</option>";
-        content += "</select></span>";
-
-        $("#monthOrYear").append(content);
+    if (!yearExists) {
+        showSelect("月", "month", 1, 12);
     } else if (yearExists) {
-        var content = "<span id='year'>請選擇年份：<select>";
-        for (var i = 2011; i <= 2016; i++)
-            content += "<option value='" + i + "'>" + i + "年</option>";
-        content += "</select></span>";
-
-        $("#monthOrYear").append(content);
+        showSelect("年", "year", 2011, 2016);
     }
 
 
@@ -53,3 +43,12 @@ $(document).ready(function() {
 
 
 });
+
+function showSelect(range, engRange, start, end) {
+    var content = "<span id='" + engRange + "'>請選擇" + range + "份：<select>";
+    for (var i = start; i <= end; i++)
+        content += "<option value='" + i + "'>" + i + range + "</option>";
+    content += "</select></span>";
+
+    $("#monthOrYear").append(content);
+}
